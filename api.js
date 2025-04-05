e5b42a76202a871b8c96b23004276f3cc0f8b5d1
FAguirre07 <lionelfaguirre@gmail.com>
2025-04-04 21:00:11 -0300

    final

--- api.js
+++ api.js
@@ -11,14 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 li.innerHTML = `
                 <img src="${producto.imagen}" alt="${producto.nombre}">
-                <div>
-                    <strong>${producto.nombre}</strong><br>
+                <div class="flex items-end w-full">
+                <div class="flex-1">
+                    <strong><b>${producto.nombre}</strong><br>
                     <span>Precio: $${producto.precio.toLocaleString()}</span>
+                    </div>
+                <div class="flex justify-end w-full"> 
+                <button class="text-[15px] tracking-[3px]
+            uppercase text-white transition-all px-8
+            bg-[#4E392B]/80"> Agregar al carrito. </button>
+            </div>
                 </div>
                 <div class="detalles">
                   ${producto.detalles ? ` <p>${producto.detalles}</p>` : ""} 
                     ${producto.extras ? ` <p>${producto.extras}</p>` : ""}
                 </div>
+
+                
             `;
                 li.addEventListener("mouseover", () => {
                     li.querySelector(".detalles").style.display = "block";
